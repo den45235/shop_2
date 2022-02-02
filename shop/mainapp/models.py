7fc7f0831edbbcb336ec585e14c7be3e0de2a1a5
den45235 <zed8iz2x@gmail.com>
2022-02-02 13:33:18 +0300

    2

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -17,3 +17,19 @@ class Product(models.Model):
     image = models.ImageField(verbose_name='изображение')
     description = models.TextField(verbose_name='описание', null=True)
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='цена')
+
+    def __str__(self):
+        return self.title
+
+class CartProduct(models.Model):
+
+    user = models.ForeignKey('customer', verbose_name='покупатель', on_delete=models.CASCADE)
+    cart = models.ForeignKey('cart', verbose_name='корзина', on_delete=models. CASCADE)
+    product = models.ForeignKey(Product, verbose_name='товар', on_delete=models.CASCADE)
+    qty = models.PositiveIntegerField(default=1)
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='общая цена')
+
+    def __str__(self):
+        return "продукт: {} (для корзины)".format(self.product.title)
+
+
